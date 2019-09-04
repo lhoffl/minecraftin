50c7550042d52ec57a025b00cc65dee061790710
Matthew Hoffman <matt@lhoffl.com>
2019-09-04 01:11:18 -0400

    Update runOverviewer.sh

--- runOverviewer.sh
+++ runOverviewer.sh
@@ -24,7 +24,7 @@ if [[ "$difference" -gt "60" ]] || [[ $1 > 0 ]]; then
 
 	/usr/bin/python3 /home/ubuntu/overviewer.py --config=/home/ubuntu/minecraftin/overviewer_conf.py
 	/usr/bin/python3 /home/ubuntu/overviewer.py --config /home/ubuntu/minecraftin/overviewer_conf.py --genpoi --skip-players
-	/usr/bin/aws s3 sync --delete /home/ubuntu/mcmap/ s3://lhoffl.com/mcmap/
+	/usr/bin/aws s3 sync --delete /home/ubuntu/mcmap/ s3://lhoffl.com/minecraftin/
 	
     /bin/date +%Y%m%d%H > $last_date_overviewer
     rm $lock_file
