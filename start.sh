fb785515241b05ac975d8b3f9920276947b1ccec
Ubuntu <ubuntu@ip-172-31-88-81.ec2.internal>
2019-09-05 20:05:20 +0000

    added pull from aws for overviewer before sync

--- start.sh
+++ start.sh
@@ -20,5 +20,3 @@ cd /home/ubuntu
 export SCREENDIR=/home/ubuntu/screens/
 chmod 700 /home/ubuntu/screens/
 /usr/bin/screen -dmS minecraft /bin/bash -c '/usr/bin/sudo /usr/bin/java -Xmx4096M -Xms4096M -jar /home/ubuntu/fabric-server-launch.jar nogui'
-
-/usr/bin/aws s3 cp s3://lhoffl.com /home/ubuntu/mcmap
