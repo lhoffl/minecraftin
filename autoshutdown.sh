ccf55c8dc582f9cfd57f72a900e47eee47a6ca0d
Ubuntu <ubuntu@ip-172-31-88-81.ec2.internal>
2019-09-06 00:38:30 +0000

    additional fixes

--- autoshutdown.sh
+++ autoshutdown.sh
@@ -2,12 +2,13 @@
 SERVICE='fabric-server-launch.jar'
 if ps ax | grep -v grep | grep $SERVICE > /dev/null; then
     PLAYERSEMPTY=" There are 0 of a max 20 players online"
-	$(screen -S minecraft -p 0 -X stuff "list^M")
+	$(/usr/bin/screen -S minecraft -p 0 -X stuff "list^M")
 	sleep 5
-	$(screen -S minecraft -p 0 -X stuff "list^M")
+	$(/usr/bin/screen -S minecraft -p 0 -X stuff "list^M")
 	sleep 5
 	PLAYERSLIST=$(tail -n 1 /home/ubuntu/logs/latest.log | cut -f2 -d"/" | cut -f2 -d":")
 	echo $PLAYERSLIST
+	/usr/bin/logger $PLAYERSLIST
 	if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ]
 	then
 		/usr/bin/logger "Waiting for players to come back in 5m, otherwise shutdown"
@@ -29,7 +30,7 @@ if ps ax | grep -v grep | grep $SERVICE > /dev/null; then
 			
             	/bin/bash /home/ubuntu/minecraftin/backup_server.sh true
 		/bin/bash /home/ubuntu/minecraftin/runOverviewer.sh
-		$(screen -S minecraft -p 0 -X stuff "say Server powering down. ^M")
+		$(/usr/bin/screen -S minecraft -p 0 -X stuff "say Server powering down. ^M")
               	/usr/bin/python3 /home/ubuntu/minecraftin/sendMessage.py "Server shutting down. Restart it at http://minecraftin.herokuapp.com/"
 		/bin/rm /home/ubuntu/minecraftin/server.lock
 			  $(sudo /sbin/shutdown -P +1)
@@ -49,7 +50,7 @@ else
           else
             /bin/bash /home/ubuntu/minecraftin/backup_server.sh true
 	    /bin/bash /home/ubuntu/minecraftin/runOverviewer.sh
-            $(screen -S minecraft -p 0 -X stuff "say Server powering down. ^M")
+            $(/usr/bin/screen -S minecraft -p 0 -X stuff "say Server powering down. ^M")
             /usr/bin/python3 /home/ubuntu/minecraftin/sendMessage.py "Server shutting down. Restart it at http://minecraftin.herokuapp.com/"
 	    /bin/rm /home/ubuntu/minecraftin/server.lock
             $(sudo /sbin/shutdown -P +1)
