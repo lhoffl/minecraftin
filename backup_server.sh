23e293e0a71939b715dfac770edb20fdf30bf6a0
Ubuntu <ubuntu@ip-172-31-88-81.ec2.internal>
2019-09-06 00:34:04 +0000

    backup now only transfers to s3 if server powering down

--- backup_server.sh
+++ backup_server.sh
@@ -1,38 +1,26 @@
 #!/bin/bash
 
-backup_date_file="/home/ubuntu/minecraftin/last_date_backup.txt"
-
-if [[ ! -f "$backup_date_file" ]]; then
-  touch "$backup_date_file"
-fi
-
-last_date=$(/bin/cat $backup_date_file)
-current=$(/bin/date +%Y%m%d%H)
-
-difference=$((current-last_date))
-echo $difference
-
 lock_file="/home/ubuntu/minecraftin/backup.lock"
 
-if [[  $difference > 60 ]] || [[ $1 > 0 ]]; then
 
-    if [[ -f "$lock_file" ]]; then
-      exit
-    fi
+if [[ -f "$lock_file" ]]; then
+	exit
+fi
 
-    touch $lock_file
+touch $lock_file
 
-    /bin/bash /home/ubuntu/minecraftin/msg_all_outputs.sh "Server backup in progress"
+/bin/bash /home/ubuntu/minecraftin/msg_all_outputs.sh "Server backup in progress"
 
-	/bin/mv /home/ubuntu/backups/latest_backup.tar.gz /home/ubuntu/backups/old_.tar.gz
-	cd /home/ubuntu/
-	/bin/tar -czvf backups/latest_backup.tar.gz world/
+/bin/mv /home/ubuntu/backups/latest_backup.tar.gz /home/ubuntu/backups/old_.tar.gz
+cd /home/ubuntu/
+/bin/tar -czvf backups/latest_backup.tar.gz world/
+
+if [[ $1 > 0 ]]; then
 	/usr/bin/aws s3 cp /home/ubuntu/backups/latest_backup.tar.gz s3://lhoffl.com/minecraftin_backups/$current-backup.tar.gz
 	/usr/bin/aws s3 rm s3://lhoffl.com/minecraftin_backups/latest_backup.tar.gz
 	/usr/bin/aws s3 cp /home/ubuntu/backups/latest_backup.tar.gz s3://lhoffl.com/minecraftin_backups/latest_backup.tar.gz
-	/bin/date +%Y%m%d%H > $backup_date_file
 
-    /bin/bash /home/ubuntu/minecraftin/msg_all_outputs.sh "Server backup completed"
-    rm $lock_file
 fi
 
+/bin/bash /home/ubuntu/minecraftin/msg_all_outputs.sh "Server backup completed"	
+rm $lock_file
