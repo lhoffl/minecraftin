b098bb47b0287a3807c9622117056849469576e3
Matthew Hoffman <matt@lhoffl.com>
2019-09-14 16:42:33 -0400

    Update autoshutdown.sh

--- autoshutdown.sh
+++ autoshutdown.sh
@@ -40,22 +40,22 @@ if ps ax | grep -v grep | grep $SERVICE > /dev/null; then
             else
                 /bin/bash /home/ubuntu/minecraftin/runOverviewer.sh
 		
-		   	PLAYERSEMPTY=" There are 0 of a max 20 players online"
-			$(/usr/bin/screen -S minecraft -p 0 -X stuff "list^M")
-			sleep 5
-			$(/usr/bin/screen -S minecraft -p 0 -X stuff "list^M")
-			sleep 5
-			PLAYERSLIST=$(tail -n 1 /home/ubuntu/logs/latest.log | cut -f2 -d"/" | cut -f2 -d":")
-			echo $PLAYERSLIST
+		   	#PLAYERSEMPTY=" There are 0 of a max 20 players online"
+			#$(/usr/bin/screen -S minecraft -p 0 -X stuff "list^M")
+			#sleep 5
+			#$(/usr/bin/screen -S minecraft -p 0 -X stuff "list^M")
+			#sleep 5
+			#PLAYERSLIST=$(tail -n 1 /home/ubuntu/logs/latest.log | cut -f2 -d"/" | cut -f2 -d":")
+			#echo $PLAYERSLIST
 			
-		if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ]; then
+		#if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ]; then
 			/bin/bash /home/ubuntu/minecraftin/backup_server.sh true
 			$(/usr/bin/screen -S minecraft -p 0 -X stuff "say Server powering down. ^M")
               		/usr/bin/python3 /home/ubuntu/minecraftin/sendMessage.py "Server shutting down. Restart it at http://minecraftin.herokuapp.com/"
 			/bin/rm /home/ubuntu/minecraftin/server.lock
-		fi
-            fi
-		fi
+		#fi
+            #fi
+	    fi
 	fi
 else
 	/usr/bin/logger "Screen does not exist briefly waiting before trying again"
