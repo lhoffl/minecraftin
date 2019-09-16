f2d1e5d378f57af9a825b076dffc031cc9a88b00
Matthew Hoffman <matt@lhoffl.com>
2019-09-16 12:43:22 -0400

    Update autoshutdown.sh

--- autoshutdown.sh
+++ autoshutdown.sh
@@ -31,31 +31,30 @@ if ps ax | grep -v grep | grep $SERVICE > /dev/null; then
 		PLAYERSLIST=$(tail -n 1 /home/ubuntu/logs/latest.log | cut -f2 -d"/" | cut -f2 -d":")
 		if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ]
 		then
-            if [[ -f "/home/ubuntu/minecraftin/overviewer.lock" ]]; then
-		      /usr/bin/logger "Overviewer run currently in progress, waiting for the run to finish before shutting down."
-              exit
-            elif [[ -f "/home/ubuntu/minecraftin/backup.lock" ]]; then
-		      /usr/bin/logger "Server backup currently in progress, waiting for the run to finish before shutting down."
-              exit
-            else
-                /bin/bash /home/ubuntu/minecraftin/runOverviewer.sh
+            		if [[ -f "/home/ubuntu/minecraftin/overviewer.lock" ]]; then
+		      		/usr/bin/logger "Overviewer run currently in progress, waiting for the run to finish before shutting down."
+              			exit
+            		elif [[ -f "/home/ubuntu/minecraftin/backup.lock" ]]; then
+		      		/usr/bin/logger "Server backup currently in progress, waiting for the run to finish before shutting down."
+              	      		exit
+            		else
+                		/bin/bash /home/ubuntu/minecraftin/runOverviewer.sh
 		
-		   	#PLAYERSEMPTY=" There are 0 of a max 20 players online"
-			#$(/usr/bin/screen -S minecraft -p 0 -X stuff "list^M")
-			#sleep 5
-			#$(/usr/bin/screen -S minecraft -p 0 -X stuff "list^M")
-			#sleep 5
-			#PLAYERSLIST=$(tail -n 1 /home/ubuntu/logs/latest.log | cut -f2 -d"/" | cut -f2 -d":")
-			#echo $PLAYERSLIST
+		   		PLAYERSEMPTY=" There are 0 of a max 20 players online"
+				$(/usr/bin/screen -S minecraft -p 0 -X stuff "list^M")
+				sleep 5	
+				$(/usr/bin/screen -S minecraft -p 0 -X stuff "list^M")
+				sleep 5
+				PLAYERSLIST=$(tail -n 1 /home/ubuntu/logs/latest.log | cut -f2 -d"/" | cut -f2 -d":")
+				echo $PLAYERSLIST
 			
-		#if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ]; then
-			/bin/bash /home/ubuntu/minecraftin/backup_server.sh true
-			$(/usr/bin/screen -S minecraft -p 0 -X stuff "say Server powering down. ^M")
-              		/usr/bin/python3 /home/ubuntu/minecraftin/sendMessage.py "Server shutting down. Restart it at http://minecraftin.herokuapp.com/"
-			/bin/rm /home/ubuntu/minecraftin/server.lock
-		#fi
-            #fi
-	    fi
+				if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ]; then
+					/bin/bash /home/ubuntu/minecraftin/backup_server.sh true
+					$(/usr/bin/screen -S minecraft -p 0 -X stuff "say Server powering down. ^M")
+					/bin/rm /home/ubuntu/minecraftin/server.lock
+				fi
+            		fi
+	    	fi
 	fi
 else
 	/usr/bin/logger "Screen does not exist briefly waiting before trying again"
@@ -70,10 +69,20 @@ else
           else
 	     /bin/bash /home/ubuntu/minecraftin/runOverviewer.sh
             /bin/bash /home/ubuntu/minecraftin/backup_server.sh true
-
-            $(/usr/bin/screen -S minecraft -p 0 -X stuff "say Server powering down. ^M")
-            /usr/bin/python3 /home/ubuntu/minecraftin/sendMessage.py "Server shutting down. Restart it at http://minecraftin.herokuapp.com/"
-	    /bin/rm /home/ubuntu/minecraftin/server.lock
+		
+		   		PLAYERSEMPTY=" There are 0 of a max 20 players online"
+				$(/usr/bin/screen -S minecraft -p 0 -X stuff "list^M")
+				sleep 5	
+				$(/usr/bin/screen -S minecraft -p 0 -X stuff "list^M")
+				sleep 5
+				PLAYERSLIST=$(tail -n 1 /home/ubuntu/logs/latest.log | cut -f2 -d"/" | cut -f2 -d":")
+				echo $PLAYERSLIST
+			
+				if [ "$PLAYERSLIST" = "$PLAYERSEMPTY" ]; then
+					/bin/bash /home/ubuntu/minecraftin/backup_server.sh true
+					$(/usr/bin/screen -S minecraft -p 0 -X stuff "say Server powering down. ^M")
+					/bin/rm /home/ubuntu/minecraftin/server.lock
+				fi
           fi
 	fi
 fi
